d8059580e829117695cf40ffe279ec1b27f4e0a5
Samuel Oliphant <oliphantsb@gmail.com>
2019-12-06 01:21:08 -0500

    memoization fix

--- day6/redemption_sol.py
+++ day6/redemption_sol.py
@@ -7,8 +7,8 @@ import math
 # I was right. Less than half the code and runs lightyears faster.
 
 def find_dist_from_com(name):
-	if name == "COM":
-		return 0
+	if name in dist_from_com:
+		return dist_from_com[name]
 	ans = 1 + find_dist_from_com(child_to_parent[name])
 	dist_from_com[name] = ans
 	return ans
@@ -16,7 +16,7 @@ def find_dist_from_com(name):
 fn = "./in.txt"
 l = [(x.split(")")[0], x.split(")")[1]) for x in util.filetowordlist(fn)]
 child_to_parent = {}
-dist_from_com = {}
+dist_from_com = {"COM": 0}
 for p in l:
 	child_to_parent[p[1]] = p[0]
 for star in child_to_parent:
