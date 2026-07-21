5c41b4d022ec9c173324190031cb33a290cb24ff
samstronghammer <sam@luna-nelson.com>
2026-07-21 15:50:09 -0400

    day 21 cleanup.

--- day21/sol.py
+++ day21/sol.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 import sys
 sys.path.append("..")
-import util
 import intcode
-import math
 
 # Interesting puzzle. I found the small number of mutable registers
 # to be very limiting. Once I figured out that "or"ing together the
 # three spots in front could be done in one register things fell
 # into place.
 
+debug_mode = False
+
 def i_to_c(i):
-	return str(chr(i))
+  return str(chr(i))
 
 def c_to_i(c):
-	return ord(c)
+  return ord(c)
 
 def get_input(s):
-	index = 0
-	while True:
-		yield c_to_i(s[index])
-		index += 1
+  for c in s:
+    yield ord(c)
 
 instr="""OR A J
 AND B J
@@ -30,18 +28,17 @@ AND D J
 WALK
 """
 
-gen = get_input(instr)
-c = intcode.Computer(get_input=gen)
+c = intcode.Computer(get_input(instr))
 while True:
-	val = c.calc()
-	if c.halted:
-		break
-	if val > 256:
-		print("Part 1")
-		print(val)
-	else:
-		print(i_to_c(val), end="")
-	
+  val = c.calc()
+  if c.halted:
+    break
+  if val > 256:
+    print("Part 1")
+    print(val)
+  elif debug_mode:
+    print(i_to_c(val), end="")
+  
 
 instr2="""OR E T
 OR H T
@@ -54,14 +51,14 @@ AND D J
 RUN
 """
 
-gen2 = get_input(instr2)
-c2 = intcode.Computer(get_input=gen2)
+c2 = intcode.Computer(get_input(instr2))
 while True:
-	val = c2.calc()
-	if c2.halted:
-		break
-	if val > 256:
-		print("Part 2")
-		print(val)
-	else:
-		print(i_to_c(val), end="")
+  val = c2.calc()
+  if c2.halted:
+    break
+  if val > 256:
+    print("Part 2")
+    print(val)
+    break
+  elif debug_mode:
+    print(i_to_c(val), end="")
