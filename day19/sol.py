84ae7daef459a8f3da6966f0bc6fcc99da29979f
samstronghammer <sam@luna-nelson.com>
2026-07-21 15:31:28 -0400

    Remove hard-coded parts of day 19, add binary search

--- day19/sol.py
+++ day19/sol.py
@@ -5,59 +5,94 @@ import util
 import intcode
 import math
 
+# Comment from initial implementation:
 # I liked this one. Forces the user to constrain the search space.
 # I found the value of 2000 for how far I had to look by guessing
 # around. Part 1 went very quickly for me.
 
-inputx = 0
-inputy = 0
-sending_x = True
+# Now it's more robustly able to binary search the search space.
 
 def get_input(x, y):
-	yield x
-	yield y
+  yield x
+  yield y
+
+def query(loc):
+  if loc[0] < 0 or loc[1] < 0:
+    return 0
+  return intcode.Computer(get_input(loc[0], loc[1])).calc()
 
 locs = {}
-tot_pulled = 0
 for x in range(0, 50):
-	for y in range(0, 50):
-		c = intcode.Computer(get_input(x, y))
-		pulled = c.calc()
-		locs[(x, y)] = pulled
-		if pulled == 1:
-			tot_pulled += 1
+  for y in range(0, 50):
+    locs[(x, y)] = query((x, y))
 
 print("Part 1")
-print(tot_pulled)
+print(sum(locs.values()))
 
-start = (7, 9)
-curr_bott = start
-curr_top = start
-botts = []
-tops = set()
+def find_beam_bottom_start():
+  y = 49
+  while True:
+    this_row = [(x, y) for x in range(50)]
+    this_row_pulled = [locs[loc] for loc in this_row]
+    if sum(this_row_pulled) > 1:
+      return this_row[this_row_pulled.index(1)]
+    y -= 1
 
-for i in range(2000):
-	botts.append(curr_bott)
-	tops.add(curr_top)
-	c_bott = intcode.Computer(get_input(curr_bott[0], curr_bott[1] + 1))
-	if c_bott.calc() == 1:
-		curr_bott = (curr_bott[0], curr_bott[1] + 1)
-	else:
-		curr_bott = (curr_bott[0] + 1, curr_bott[1])
-	c_top = intcode.Computer(get_input(curr_top[0] + 1, curr_top[1]))
-	if c_top.calc() == 1:
-		curr_top = (curr_top[0] + 1, curr_top[1])
-	else:
-		curr_top = (curr_top[0], curr_top[1] + 1)
-
-ans = None
-i = 0
 square_size = 100
-while ans == None:
-	if util.vecadd((square_size - 1, 1 - square_size), botts[i]) in tops:
-		ans = util.vecadd((0, 1 - square_size), botts[i])
-	else:
-		i += 1
 
+move_right = (1, 0)
+move_left = (-1, 0)
+
+def scan_for_bottom(close_to_bottom):
+  val = query(close_to_bottom)
+  if val == 0:
+    return scan_for_bottom(util.vecadd(close_to_bottom, move_right))
+    # move right
+  left = query(util.vecadd(close_to_bottom, move_left))
+  if left == 0:
+    return close_to_bottom
+  return scan_for_bottom(util.vecadd(close_to_bottom, move_left))
+
+def calc_top_pulled(bottom):
+  top = util.vecadd((square_size - 1, 1 - square_size), bottom)
+  return query(top)
+
+min_bottom = find_beam_bottom_start()
+max_bottom = scan_for_bottom(util.vecadd(min_bottom, min_bottom))
+while True:
+  top_pulled = calc_top_pulled(max_bottom)
+  if top_pulled == 1:
+    break
+  min_bottom = max_bottom
+  max_bottom = scan_for_bottom(util.vecadd(max_bottom, max_bottom))
+
+def calc_mid_bottom(min_bottom_loc, max_bottom_loc):
+  double_loc = util.vecadd(min_bottom_loc, max_bottom_loc)
+  close_loc = (math.floor(double_loc[0] / 2), math.floor(double_loc[1] / 2))
+  return scan_for_bottom(close_loc)
+
+while True:
+  mid_bottom = calc_mid_bottom(min_bottom, max_bottom)
+  if mid_bottom == min_bottom or mid_bottom == max_bottom:
+    break
+  top_pulled = calc_top_pulled(mid_bottom)
+  if top_pulled == 1:
+    max_bottom = mid_bottom
+  else:
+    min_bottom = mid_bottom
+
+# Jump back 1% to not miss a rounding problem
+curr_bottom = tuple(math.floor(x * 0.99) for x in min_bottom) 
+while True:
+  top_pulled = calc_top_pulled(curr_bottom)
+  if top_pulled == 1:
+    break
+  next_bottom_pulled = intcode.Computer(get_input(curr_bottom[0], curr_bottom[1] + 1)).calc()
+  if next_bottom_pulled == 1:
+    curr_bottom = (curr_bottom[0], curr_bottom[1] + 1)
+  else:
+    curr_bottom = (curr_bottom[0] + 1, curr_bottom[1])
+
+ans = util.vecadd((0, 1 - square_size), curr_bottom)
 print("Part 2")
 print(ans[0] * 10000 + ans[1])
